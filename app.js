f389bb120414c0a031e3dc3369530e53d2873410
qadevopsgdr <qadevopsgdr@gmail.com>
2025-11-17 12:15:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Hello from ECS..DR with pipeline...Monday - 17th Nov.. ..</h1>"));
+app.get("/", (req, res) => res.send("<h1>Hello from ECS..DR with pipeline...Monday - 17th Nov..2nd time deployment ..</h1>"));
 
 app.listen(80, () => {
   console.log("App listening on port 80!");
