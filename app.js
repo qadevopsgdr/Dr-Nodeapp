7f2a2181d363098422405412a9bcd5a12dcded33
qadevopsgdr <qadevopsgdr@gmail.com>
2025-11-17 12:35:10 +0530

    Action files cleaned - commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Hello from ECS..DR with pipeline...Cleaned action files...</h1>"));
+app.get("/", (req, res) => res.send("<h1>Hello from ECS..DR with pipeline...Cleaned action files at 12:30 pm..</h1>"));
 
 app.listen(80, () => {
   console.log("App listening on port 80!");
