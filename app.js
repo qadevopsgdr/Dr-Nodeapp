51145aa80c67a3c7562dfcbe960a545011d5a42d
qadevopsgdr <qadevopsgdr@gmail.com>
2025-11-17 13:06:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Hello from ECS..DR with pipeline...Cleaned action files at 12:30 pm.. Removed all old images and deploying</h1>"));
+app.get("/", (req, res) => res.send("<h1>Hello from ECS..DR with pipeline...Cleaned action files at 12:30 pm.. Removed all old images and deploying.. Old images are deleted and removing containers..</h1>"));
 
 app.listen(80, () => {
   console.log("App listening on port 80!");
