c6ceded31f95c7f9e3abad05056862bccfab4c62
qadevopsgdr <qadevopsgdr@gmail.com>
2025-11-17 13:41:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Hello from ECS..DR with pipeline...Cleaned action files at 12:30 pm.. Removed all old images and deploying.. Old images are deleted and removing containers..</h1>"));
+app.get("/", (req, res) => res.send("<h1> Hi, This is sample Node JS application. Pushed image to AWS ECR and deployed on EC2 Instance...1st version..</h1>"));
 
 app.listen(80, () => {
   console.log("App listening on port 80!");
