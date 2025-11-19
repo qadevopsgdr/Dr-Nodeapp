6b9f994f8aeaf4cf01c529fc65e201c88d06f7f0
qadevopsgdr <qadevopsgdr@gmail.com>
2025-11-19 10:59:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1> Hi, deploying with t2.micro...on nov 18th at 11:40.. </h1>"));
+app.get("/", (req, res) => res.send("<h1> Hi, deploying with t2.micro...on nov 19th 2025...at 11:00 </h1>"));
 
 app.listen(80, () => {
   console.log("App listening on port 80!");
